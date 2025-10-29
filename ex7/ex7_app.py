a0bf59748a1291f16a32f41edfcead170dac5700
Nikita Krivcov <krivcovnikita35@gmail.com>
2025-10-29 08:46:14 +0300

    feat: new commit

--- ex7/ex7_app.py
+++ ex7/ex7_app.py
@@ -4,13 +4,24 @@ from ex7.models.models import Cred
 
 app = FastAPI()
 
-user = Cred(email="kr@gmail.com", password="12345")
+USERS = {
+    'user1': User(username='user1', password='pass1'),
+    'user2': User(username='user2', password='pass2')
+}
 
-@app.post("/login")
-async def login(cred: Cred):
-    if cred.email == user.email and cred.password == user.password:
-        print("Hello")
-    else:
-        return Response(
 
+def auth_user(creds: HTTPBasicCredentials = Depends(security)):
+    user = USERS.get(creds.username)
+    if user and safe_comp(user.password, creds.password):
+        return user
+
+    raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
         )
+
+
+@app.get('/login', dependencies=[Depends(auth_user)])
+async def auth():
+    return 'You got my secret, welcome'
